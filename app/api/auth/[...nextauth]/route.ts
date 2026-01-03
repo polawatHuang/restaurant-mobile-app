e7a6e7dd8e38f5c8f3ada02eae3c4ec6ff928848
Polawat Huang <polawat_hua@tdem.toyota-asia.com>
2026-01-03 15:13:43 +0700

    Fix bug on vercel build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+// 1. Force dynamic behavior
+export const dynamic = "force-dynamic";
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
